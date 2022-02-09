cf07d30defdc1c33bd72832aed288a77bfb55fc5
dongzhongfeng <dongzhongfeng@noxgroup.com>
2022-02-09 10:36:38 +0800

    Feat:开启ViewBinding

--- app/src/main/java/com/rabbit/gradienttextview/MainActivity.java
+++ app/src/main/java/com/rabbit/gradienttextview/MainActivity.java
@@ -1,14 +1,18 @@
 package com.rabbit.gradienttextview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rabbit.gradienttextview.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
